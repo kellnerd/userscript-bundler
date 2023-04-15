8d21d35a4419d3e1a0ef665669a2bf57798783a8
David Kellner <52860029+kellnerd@users.noreply.github.com>
2023-04-15 16:28:05 +0200

    Rename package and adapt it to its new scope
    The new @kellnerd/userscript-bundler package has been split off from the
    original @kellnerd/musicbrainz-scripts package using git filter-repo to
    drop unnecessary files and interactive rebases to drop some irrelevant
    commits and reword inappropriate commit messages.
    
    From the bundler's perspective, the development dependencies become
    regular dependencies.

--- build.js
+++
@@ -1,8 +0,0 @@
-import { build } from './tools/build.js';
-
-build({
-	bookmarkletBasePath: 'src/bookmarklets',
-	userscriptBasePath: 'src/userscripts',
-	docBasePath: 'doc',
-	debug: process.argv.includes('-d'),
-});
